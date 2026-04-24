ada6b3b89882ef665303d0f677d4ba6e0a13cec4
Phelipe Bistaffa <contato.phelipebistaffa@gmail.com>
2026-04-24 11:30:59 -0400

    CrossOrigin arrumado

--- src/main/java/PhelipeProject/ChatOnlineWithWebSocket/controller/ControlerGetAll.java
+++ src/main/java/PhelipeProject/ChatOnlineWithWebSocket/controller/ControlerGetAll.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "https://phelipeprojectchat-tau.vercel.app/")
+@CrossOrigin(origins = "https://phelipeprojectchat-tau.vercel.app",methods = {RequestMethod.GET,RequestMethod.DELETE})
 public class ControlerGetAll {
 
     private final MessageRepository messageRepository;
